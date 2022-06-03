e47dae2524f0f8416f7017a64a67ac322c7361e5
Leonardo <leo_lopes09@hotmail.com>
2022-06-03 00:01:47 -0300

    Valida e formata arquivo terraform

--- 01-terraform-basico/01-primeiro-script/main.tf
+++ 01-terraform-basico/01-primeiro-script/main.tf
@@ -1,15 +1,17 @@
 provider "aws" {
-    region = "sa-east-1"
-    
+  region = "sa-east-1"
+
 }
 
 resource "aws_s3_bucket" "primeiro-bucket" {
-    bucket = "meu-primeiro-bucket-s3"
-    acl = "private"
+  bucket = "meu-primeiro-bucket-s3"
+  acl    = "private"
 
-    tags = {
-        Name = "Meu primeiro bucket S3"
-        Environment = "Dev"
-        ManagedBy = "Terraform"
-    }
+  tags = {
+    Name        = "Meu primeiro bucket S3"
+    Environment = "Dev"
+    ManagedBy   = "Terraform"
+    Owner       = "Leonardo Lopes Silva"
+    UpdatedAt   = "2022-06-02"
+  }
 }
