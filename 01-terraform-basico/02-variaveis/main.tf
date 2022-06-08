124a93e5daf51fe13142bd79e65a4853d428f2ee
Leonardo <leo_lopes09@hotmail.com>
2022-06-07 23:12:32 -0300

    Utiliza variáveis do terraform e aplica em crição de máquina virtual EC2

---
+++ 01-terraform-basico/02-variaveis/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = var.aws_region
+
+}
+
+resource "aws_instance" "instancia_ec2" {
+    ami = var.instance_ami
+    instance_type = var.instance_type
+
+    tags = var.instance_tags
+}
---
+++ 01-terraform-basico/02-variaveis/variables.tf
@@ -0,0 +1,26 @@
+variable "aws_region" {
+  type = string
+  description = "Região onde o recurso será provisionado"
+  default = "sa-east-1"
+}
+
+variable "instance_ami" {
+  type = string
+  description = "AMI na qual a máquina virtual se baseará quando for criada"
+  default = "ami-0800f9916b7655289"
+}
+
+variable "instance_type" {
+  type = string
+  description = "Tipo da instância EC2"
+  default = "t3.micro"
+}
+
+variable "instance_tags" {
+  type = map(string)
+  description = "Tags para identificação da instância"
+  default = {
+      Name = "Ubuntu"
+      Project = "Curso AWS com Terraform"
+  }
+}
