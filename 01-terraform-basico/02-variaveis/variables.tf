f2aaff0381ceeaa979f63907c485b39d31d58ba8
Leonardo <leo_lopes09@hotmail.com>
2022-06-07 23:32:54 -0300

    Cria arquivo de variáveis .tfvars | Remove prop default para tornar variáveis obrigatórias

--- 01-terraform-basico/02-variaveis/variables.tf
+++ 01-terraform-basico/02-variaveis/variables.tf
@@ -1,19 +1,21 @@
+variable "environment" {
+  type        = string
+  description = "Ambiente de execução"
+}
+
 variable "aws_region" {
   type        = string
   description = "Região onde o recurso será provisionado"
-  default     = "sa-east-1"
 }
 
 variable "instance_ami" {
   type        = string
   description = "AMI na qual a máquina virtual se baseará quando for criada"
-  default     = "ami-0800f9916b7655289"
 }
 
 variable "instance_type" {
   type        = string
   description = "Tipo da instância EC2"
-  default     = "t3.micro"
 }
 
 variable "instance_tags" {
